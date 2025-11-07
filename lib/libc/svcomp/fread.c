4ff1349241f874b6ea6d1f46a6cf5375eff88de6
Paulína Ayaziová <paulina.ayazi@gmail.com>
2025-11-07 16:07:21 +0100

    Remove unused variables and use correct nondet function

--- lib/libc/svcomp/fread.c
+++ lib/libc/svcomp/fread.c
@@ -7,15 +7,10 @@ extern _Bool __symbiotic_nondet__Bool();
 
 size_t fread(void * buf, size_t size, size_t count, FILE * fp)
 {
-	size_t resid;
-	char *p;
-	int r;
-	size_t total;
-
-	if ((count == 0) || (size == 0))
-		return (0);
+    if ((count == 0) || (size == 0))
+        return (0);
     for (size_t i = 0; i < count * size; i++) {
-        if (symbiotic_nondet__Bool())
+        if (__symbiotic_nondet__Bool())
             return i / size;
         ((char*)buf)[i] = __symbiotic_nondet_char();
     }
