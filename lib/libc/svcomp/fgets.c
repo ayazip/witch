94ee508e4e4eca23630ea5cdaee7dff95531a387
Paulína Ayaziová <paulina.ayazi@gmail.com>
2025-11-07 16:03:45 +0100

    Improve formatting

--- lib/libc/svcomp/fgets.c
+++ lib/libc/svcomp/fgets.c
@@ -6,14 +6,14 @@ extern void klee_warning_once(const char *);
 void klee_silent_exit(int) __attribute__((noreturn));
 
 char* fgets(char* str, int count, FILE *f) {
-	if (count <= 1)
-		return (NULL);
+    if (count <= 1)
+        return NULL;
 
     if (__symbiotic_nondet__Bool())
-		return NULL;
+        return NULL;
 
     size_t i;
-	for (i = 0; i < count - 1; i++) {
+    for (i = 0; i < count - 1; i++) {
 
         if (__symbiotic_nondet__Bool()) {
             str[i] = '\n';
@@ -27,5 +27,5 @@ char* fgets(char* str, int count, FILE *f) {
         str[i] = __symbiotic_nondet_char();
     }
     str[i] = 0;
-	return str;
+    return str;
 }
